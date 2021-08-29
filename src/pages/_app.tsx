1a117c636034162cc699a6fdefe167cabb9813bb
Victor Tsang <victorwltsang@gmail.com>
2021-08-29 12:20:57 -0700

    added axe a11y

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { AppProps } from 'next/app';
 import GlobalStyles from '@theme/GlobalStyles';
+
+/**
+ * Determines if we are running on server or in client.
+ * @return {boolean} true if running on server
+ */
+function getIsServerRendered() {
+  return typeof window === 'undefined';
+}
+
+/**
+ * Accessibility tool - outputs to devtools console on dev only and client-side only.
+ * @see https://github.com/dequelabs/axe-core-npm
+ */
+if (process.env.NODE_ENV !== 'production' && !getIsServerRendered()) {
+  const React = require('react');
+  const ReactDOM = require('react-dom');
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
