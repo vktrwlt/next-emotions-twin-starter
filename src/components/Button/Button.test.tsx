0bb2f181e85a62172ade65d4065d043317218517
Victor Tsang <victorwltsang@gmail.com>
2021-08-29 12:14:16 -0700

    second commit

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,7 +2,7 @@ import { Button } from '@components/Button';
 import { render, fireEvent } from '@utils/test-utils';
 
 describe('<Button />', () => {
-  const { asFragment } = render(<Button />);
+  const { asFragment } = render(<Button>Button</Button>);
   it('should render without crashing', () => {
     expect(asFragment()).toMatchSnapshot();
   });
